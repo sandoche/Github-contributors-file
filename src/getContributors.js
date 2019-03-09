9f2c63bd0836ee981505cf4402d87e95415b3218
Sandoche Adittane <sandoche@protonmail.com>
2019-03-09 17:43:35 +0100

    :rotating_light: Fixing some lint problems

--- src/getContributors.js
+++ src/getContributors.js
@@ -1,30 +1,37 @@
 export default function getContributors(owner, repository, filePath) {
   return new Promise((resolve, reject) => {
     const url = `https://api.github.com/repos/${owner}/${repository}/commits?path=${filePath}`;
+
     fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      const authorsLogin = [];
-      const contributors = json.map((item) => {
-        let result = null;
-        if (!authorsLogin.includes(item.author.login)) {
-          result = {
-            login: item.author.login,
-            avatar_url: item.author.avatar_url,
-            url: item.author.html_url
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        const authorsLogin = [];
+
+        const contributors = json.map((item) => {
+          let result = null;
+
+          /* eslint-disable */
+          if (!authorsLogin.includes(item.author.login)) {
+            result = {
+              login: item.author.login,
+              avatar_url: item.author.avatar_url,
+              url: item.author.html_url
+            };
           }
-        }
-        authorsLogin.push(item.author.login);
-        return result;
-      }).filter(item => {
-        return item != null;
+          /* eslint-enable */
+
+          authorsLogin.push(item.author.login);
+          return result;
+        }).filter(item => {
+          return item != null;
+        });
+
+        resolve(contributors);
+      })
+      .catch((e) => {
+        reject(e);
       });
-      resolve(contributors);
-    })
-    .catch((e) => {
-      reject(e)
-    });
-  })
+  });
 }
